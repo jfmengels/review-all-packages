1469ef29d20c6f4230f0ea93b1673ff6681289db
Jeroen Engels <jfm.engels@gmail.com>
2020-06-06 12:27:37 +0200

    Prepare it a bit for public

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const child_process = require('child_process');
 
 let packages = require('./search.json')
   // .filter(pkg => !pkg.name.startsWith("elm/"))
+  // TODO Stop filtering for ianmackenzie. It's
   .filter(pkg => pkg.name.startsWith("ianmackenzie/"))
   .filter(pkg => !pkg.name.startsWith("ianmackenzie/elm-3d-camera"))
 
@@ -11,13 +12,13 @@ console.log(packages.map(pkg => pkg.name));
 // packages = [packages[0]]
 packages.forEach(pkg => {
   try {
+    // TODO Pull if the package already exists?
     child_process.execSync(`git clone https://github.com/${pkg.name} packages/${pkg.name}`);
   } catch(error){}
   try {
-    child_process.execSync(`LOCAL_ELM_REVIEW_SRC=/home/jeroen/dev/elm-review/src ~/dev/node-elm-review/bin/elm-review --config /home/jeroen/dev/elm-review/review/`
-      , {cwd : path.join(__dirname, 'packages', pkg.name)}
-    );
-    console.log(`${pkg.name}: OK`);
+    // const command = `LOCAL_ELM_REVIEW_SRC=/path-to-elm-review/src ~/dev/node-elm-review/bin/elm-review --config /path-to-review-folder/`;
+    const command = `elm-review --config /path-to-review-folder/`;
+    child_process.execSync(command, {cwd : path.join(__dirname, 'packages', pkg.name)});
   } catch(error){
     console.log(`${pkg.name}: FAILURE`);
     console.log('--------------- STDOUT ------------------');
